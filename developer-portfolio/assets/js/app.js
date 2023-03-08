eb4f8b82bd29eb5b10d2fda04a309c9b7e45404f
Daniel Jaramillo <jaramillob93@gmail.com>
2023-03-07 21:19:39 -0500

    Add tailwind. Menu under construction

--- developer-portfolio/assets/js/app.js
+++ developer-portfolio/assets/js/app.js
@@ -2,22 +2,23 @@
 const scrollUp = document.querySelector('#scroll-up');
 
 // Nav Hamburguer selections
-const burguer = document.querySelector('#burguer-menu');
+const burguer = document.querySelector('#side-menu');
 const ul = document.querySelector('nav ul');
 const nav = document.querySelector('nav');
 
 // Select nav links
 const navLink = document.querySelectorAll('.nav-link');
 
+const toggleButton = document.getElementById('toggle-button');
+
 // Listeners
 navLink.forEach((link) => {
     link.addEventListener('click', () => {
-        ul.classList.remove('show');
-    })
-})
-burguer.addEventListener('click', () => {
-    ul.classList.toggle('show');
-})
+        // Toggle items
+        ul.classList.toggle('hide');
+        if (ul.classList.contains('hide')) nav.style.minHeight = 0;
+    });
+});
 
 scrollUp.addEventListener('click', () => {
     window.scrollTo({
@@ -27,7 +28,32 @@ scrollUp.addEventListener('click', () => {
     });
 });
 
+toggleButton.addEventListener('change', () => {
+    document.body.classList.toggle('dark');
+
+    const isInDarkMode = document.body.classList.contains('dark');
+    toggleDarkModeStyles(isInDarkMode);
+});
+
 function loadContent() {
     document.querySelector('#loader').style.display = 'none';
     document.querySelector('#container').style.display = 'block';
-}
+};
+
+function toggleDarkModeStyles(isDark) {
+    const navBackground = isDark ? "#212529" : "f8f9fa";
+    const headerBackground = isDark ? "#212529" : "#f8f9fa";
+    const hambLineBackground = isDark ? "#f8f9fa" : "#212529";
+    const hambLineElements = document.querySelectorAll('.hamb-line');
+    const sheet = new CSSStyleSheet();
+
+    nav.style.background = navBackground;
+    document.getElementsByClassName('header')[0].style.background = headerBackground;
+    [].forEach.call(hambLineElements, (hambLine) => { 
+        hambLine.style.setProperty("background", hambLineBackground) });
+    sheet.replace(
+        `span.hamb-line::after { background: ${hambLineBackground}; } 
+        span.hamb-line::before { background: ${hambLineBackground}; }`)
+        .then(() => {});
+    document.adoptedStyleSheets = [sheet];
+};
---
+++ developer-portfolio/tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./assets/*.{html}'],
+  theme: {
+    colors: {
+      'bg-color': '#FCFCFC',
+      'primary-color': '#4756DF',
+      'secondary-color': '#FF7235',
+      'primary-shadow': '#8B8EAF',
+      'secondary-shadow': '#A17A69',
+      'light-0': '#f8f9fa',
+      'light-1': '#e9ecef',
+      'light-2': '#dee2e6',
+      'dark-0': '#495057',
+      'dark-1': '#343a40',
+      'dark-2': '#212529'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
